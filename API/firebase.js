402424940d6537556fc832558668546de127a334
Laney E. Humble <humblel@etown.edu>
2026-02-19 11:35:41 -0500

    making one big API folder

--- API/firebase.js
+++ API/firebase.js
@@ -5,11 +5,7 @@
  *  ./util/firebase_connection.js file. 
  * **/
 
-//TODO:
-// make one API folder
-// work on getAssignments stuff
-//move firebase.js to API folder and redownload .env to that folder
-// npm install dotenv
+
 
 // Central Firebase initialization and helpers
 
